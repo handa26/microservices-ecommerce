05f4f5a12b42790e8839b0276c9ed2193140dc9d
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-04-21 15:10:31 +0800

    fix: env not reading values

--- packages/product-db/src/client.ts
+++ packages/product-db/src/client.ts
@@ -1,8 +1,14 @@
 import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
+const connectionString = process.env.DATABASE_URL as string;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
 const adapter = new PrismaPg({
-	connectionString: process.env.DATABASE_URL,
+	connectionString,
 });
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
