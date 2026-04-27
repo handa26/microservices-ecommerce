0d51e00b950836ad2c40f4c8d18fd747342ffeb9
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-04-27 09:20:09 +0800

    refactor: comment unnecessary types

--- apps/client/src/types.ts
+++ apps/client/src/types.ts
@@ -1,39 +1,39 @@
 import { z } from "zod";
 
-export type ProductType = {
-	id: number | string;
-	name: string;
-	shortDescription: string;
-	description: string;
-	price: number;
-	sizes: [string, ...string[]];
-	colors: [string, ...string[]];
-	images: Record<string, string>;
-};
+// export type ProductType = {
+// 	id: number | string;
+// 	name: string;
+// 	shortDescription: string;
+// 	description: string;
+// 	price: number;
+// 	sizes: [string, ...string[]];
+// 	colors: [string, ...string[]];
+// 	images: Record<string, string>;
+// };
 
-export type ProductsType = ProductType[];
+// export type ProductsType = ProductType[];
 
-export type CartItemType = ProductType & {
-	quantity: number;
-	selectedSize: string;
-	selectedColor: string;
-};
+// export type CartItemType = ProductType & {
+// 	quantity: number;
+// 	selectedSize: string;
+// 	selectedColor: string;
+// };
 
-export type CartItemsType = CartItemType[];
+// export type CartItemsType = CartItemType[];
 
-export const shippingFormSchema = z.object({
-	name: z.string().min(3, "Name is required!"),
-	email: z.email().min(3, "Email is required!"),
-	phone: z
-		.string()
-		.min(7, "Phone number must be between 7 and 10 digits!")
-		.max(10, "Phone number must be between 7 and 10 digits!")
-		.regex(/^\d+$/, "Phone number must be digits only!"),
-	address: z.string().min(1, "Address is required!"),
-	city: z.string().min(1, "City is required!"),
-});
+// export const shippingFormSchema = z.object({
+// 	name: z.string().min(3, "Name is required!"),
+// 	email: z.email().min(3, "Email is required!"),
+// 	phone: z
+// 		.string()
+// 		.min(7, "Phone number must be between 7 and 10 digits!")
+// 		.max(10, "Phone number must be between 7 and 10 digits!")
+// 		.regex(/^\d+$/, "Phone number must be digits only!"),
+// 	address: z.string().min(1, "Address is required!"),
+// 	city: z.string().min(1, "City is required!"),
+// });
 
-export type ShippingFormInputs = z.infer<typeof shippingFormSchema>;
+// export type ShippingFormInputs = z.infer<typeof shippingFormSchema>;
 
 export const paymentFormSchema = z.object({
 	cardHolder: z.string().min(3, "Card holder is required!"),
@@ -49,13 +49,13 @@ export const paymentFormSchema = z.object({
 
 export type PaymentFormInputs = z.infer<typeof paymentFormSchema>;
 
-export type CartStoreStateType = {
-	cart: CartItemsType;
-	hasHydrated: boolean;
-};
+// export type CartStoreStateType = {
+// 	cart: CartItemsType;
+// 	hasHydrated: boolean;
+// };
 
-export type CartStoreActionsType = {
-	addToCart: (product: CartItemType) => void;
-	removeFromCart: (product: CartItemType) => void;
-	clearCart: () => void;
-};
+// export type CartStoreActionsType = {
+// 	addToCart: (product: CartItemType) => void;
+// 	removeFromCart: (product: CartItemType) => void;
+// 	clearCart: () => void;
+// };
