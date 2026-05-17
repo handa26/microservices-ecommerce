544fd700db7654c937dc91af3a632eb5c7bde98f
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-05-17 15:52:29 +0800

    feat: configure stripe payment

--- apps/payment-service/src/index.ts
+++ apps/payment-service/src/index.ts
@@ -3,10 +3,32 @@ import { Hono } from "hono";
 import { clerkMiddleware } from "@hono/clerk-auth";
 
 import { shouldBeUser } from "./middlewares/authMiddleware.js";
+import stripe from "./utils/stripe.js";
 
 const app = new Hono();
 
 app.use("*", clerkMiddleware());
+
+app.post("/create-stripe-product", async (c) => {
+	const res = await stripe.products.create({
+		id: "123",
+		name: "Testing Product",
+		default_price_data: {
+			currency: "usd",
+			unit_amount: 10 * 100,
+		},
+	});
+
+	return c.json(res);
+});
+
+app.get("/stripe-product-price", async (c) => {
+	const res = await stripe.prices.list({
+		product: "123",
+	});
+
+	return c.json(res);
+});
 
 app.get("/health", (c) => {
 	return c.json({
