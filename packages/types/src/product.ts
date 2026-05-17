a13daeb94a33d1823e04a4dc89024df9775ca8c7
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-05-18 07:16:48 +0800

    feat: add another product types

--- packages/types/src/product.ts
+++ packages/types/src/product.ts
@@ -1,5 +1,11 @@
+import type { Product, Category } from "@repo/product-db";
+
+export type ProductType = Product;
+
 export type StripeProductType = {
 	id: string;
 	name: string;
 	price: number;
 };
+
+export type CategoryType = Category;
