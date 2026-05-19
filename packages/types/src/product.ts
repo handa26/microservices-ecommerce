9cf554b685af02ec5c459654c0d051bbace50af4
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-05-19 19:02:49 +0800

    feat: add additional cart & product types

--- packages/types/src/product.ts
+++ packages/types/src/product.ts
@@ -2,6 +2,8 @@ import type { Product, Category } from "@repo/product-db";
 
 export type ProductType = Product;
 
+export type ProductsType = ProductType[];
+
 export type StripeProductType = {
 	id: string;
 	name: string;
