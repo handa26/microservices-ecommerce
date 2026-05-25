7e353486f9b85f669d9a87277a791a85c901689a
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-05-25 19:08:45 +0800

    refactor: remove stripe api version

--- apps/payment-service/src/utils/stripe.ts
+++ apps/payment-service/src/utils/stripe.ts
@@ -1,7 +1,9 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-	apiVersion: "2025-08-27.basil" as any,
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+// {
+// 	apiVersion: "2025-08-27.basil" as any,
+// }
 
 export default stripe;
