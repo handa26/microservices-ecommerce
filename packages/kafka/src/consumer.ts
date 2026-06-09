45c6b47d55bcad19f1fceec1c5c4fefa79e0573a
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-06-09 19:02:43 +0800

    fix: kafka subscribe accept only 1 topic instead of multiple topics

--- packages/kafka/src/consumer.ts
+++ packages/kafka/src/consumer.ts
@@ -1,40 +1,45 @@
 import type { Kafka, Consumer } from "kafkajs";
 
 export const createConsumer = (kafka: Kafka, groupId: string) => {
-	const consumer: Consumer = kafka.consumer({ groupId });
+  const consumer: Consumer = kafka.consumer({ groupId });
 
-	const connect = async () => {
-		await consumer.connect();
-		console.log(`Kafka consumer connected: ${groupId}`);
-	};
+  const connect = async () => {
+    await consumer.connect();
+    console.log("Kafka consumer connected:" + groupId);
+  };
 
-	const subscribe = async (
-		topic: string,
-		handler: (message: any) => Promise<void>,
-	) => {
-		await consumer.subscribe({
-			topic,
-			fromBeginning: true,
-		});
+  const subscribe = async (
+    topics: {
+      topicName: string;
+      topicHandler: (message: any) => Promise<void>;
+    }[]
+  ) => {
+    await consumer.subscribe({
+      topics: topics.map((topic) => topic.topicName),
+      fromBeginning: true,
+    });
 
     await consumer.run({
       eachMessage: async ({ topic, partition, message }) => {
         try {
-          const value = message.value?.toString();
+          const topicConfig = topics.find((t) => t.topicName === topic);
+          if (topicConfig) {
+            const value = message.value?.toString();
 
-          if (value) {
-            await handler(JSON.parse(value));
+            if (value) {
+              await topicConfig.topicHandler(JSON.parse(value));
+            }
           }
         } catch (error) {
-          console.log("Error processing kafka message.", error);
+          console.log("Error processing message", error);
         }
-      }
-    })
-	};
+      },
+    });
+  };
 
-	const disconnect = async () => {
-		await consumer.disconnect();
-	};
+  const disconnect = async () => {
+    await consumer.disconnect();
+  };
 
-	return { connect, subscribe, disconnect };
+  return { connect, subscribe, disconnect };
 };
