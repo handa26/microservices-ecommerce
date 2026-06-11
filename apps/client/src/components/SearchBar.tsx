f64d4462fa703e71f2579cd5a71b491dc1490c63
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-06-11 15:21:29 +0800

    feat: implement search products filter

--- apps/client/src/components/SearchBar.tsx
+++ apps/client/src/components/SearchBar.tsx
@@ -1,6 +1,22 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 import { Search } from "lucide-react";
 
 const SearchBar = () => {
+	const [value, setValue] = useState("");
+
+	const searchParams = useSearchParams();
+	const router = useRouter();
+
+	const handleSearch = (value: string) => {
+		const params = new URLSearchParams(searchParams);
+		params.set("search", value);
+
+		router.push(`/products?${params.toString()}`, { scroll: false });
+	};
+
 	return (
 		<div className="hidden sm:flex items-center gap-2 rounded-md ring-1 ring-gray-200 px-2 py-1 hover:shadow-md">
 			<Search className="w-4 h-4 text-gray-500" />
@@ -8,6 +24,12 @@ const SearchBar = () => {
 				id="search"
 				placeholder="Search..."
 				className="text-sm outline-0"
+				onChange={(e) => setValue(e.target.value)}
+				onKeyDown={(e) => {
+					if (e.key === "Enter") {
+						handleSearch(value);
+					}
+				}}
 			/>
 		</div>
 	);
