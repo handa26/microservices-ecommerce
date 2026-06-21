0a61c89fab5553532307356cfd3dd3889b2d7c79
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-06-21 08:50:36 +0800

    feat: create new user

--- apps/admin/src/components/AddUser.tsx
+++ apps/admin/src/components/AddUser.tsx
@@ -3,135 +3,191 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import { useAuth } from "@clerk/nextjs";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-toastify";
 
 import {
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
+	SheetContent,
+	SheetDescription,
+	SheetHeader,
+	SheetTitle,
 } from "@/components/ui/sheet";
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
 } from "./ui/form";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 
+import { UserFormSchema } from "@repo/types";
+
 const formSchema = z.object({
-  fullname: z
-    .string()
-    .min(2, { message: "Fullname must be at least 2 characters!" })
-    .max(50),
-  email: z.string().email({ message: "Invalid email address!" }),
-  phone: z.string().min(10).max(15),
-  address: z.string().min(2),
-  city: z.string().min(2),
+	fullname: z
+		.string()
+		.min(2, { message: "Fullname must be at least 2 characters!" })
+		.max(50),
+	email: z.string().email({ message: "Invalid email address!" }),
+	phone: z.string().min(10).max(15),
+	address: z.string().min(2),
+	city: z.string().min(2),
 });
 
 const AddUser = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-  });
-  
-  return (
-    <SheetContent>
-      <SheetHeader>
-        <SheetTitle className="mb-4">Add User</SheetTitle>
-        <SheetDescription asChild>
-          <Form {...form}>
-            <form className="space-y-8">
-              <FormField
-                control={form.control}
-                name="fullname"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Fullname</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormDescription>
-                      Enter your fullname.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormDescription>
-                      Only admin can see your email.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="phone"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Phone</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormDescription>
-                      Only admin can see your phone number (optional).
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="address"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Address</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormDescription>
-                      Enter user address (optional).
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="city"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>City</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormDescription>
-                      Enter user city (optional).
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit">Submit</Button>
-            </form>
-          </Form>
-        </SheetDescription>
-      </SheetHeader>
-    </SheetContent>
-  );
+	const { getToken } = useAuth();
+
+	const form = useForm<z.infer<typeof UserFormSchema>>({
+		resolver: zodResolver(UserFormSchema),
+		defaultValues: {
+			firstName: "",
+			lastName: "",
+			emailAddress: [],
+			username: "",
+			password: "",
+		},
+	});
+
+	const mutation = useMutation({
+		mutationFn: async (data: z.infer<typeof UserFormSchema>) => {
+			const token = await getToken();
+
+			const res = await fetch(
+				`${process.env.NEXT_PUBLIC_AUTH_SERVICE_URL}/users`,
+				{
+					method: "POST",
+					body: JSON.stringify(data),
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${token}`,
+					},
+				},
+			);
+
+			if (!res.ok) {
+				throw new Error("Failed to create new user.");
+			}
+		},
+		onSuccess: () => {
+			toast.success("User created successfully.");
+		},
+		onError: (error) => {
+			toast.error(error.message);
+		},
+	});
+
+	return (
+		<SheetContent>
+			<SheetHeader>
+				<SheetTitle className="mb-4">Add User</SheetTitle>
+				<SheetDescription asChild>
+					<Form {...form}>
+						<form
+							className="space-y-8"
+							onSubmit={form.handleSubmit((data) => mutation.mutate(data))}
+						>
+							<FormField
+								control={form.control}
+								name="firstName"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>First Name</FormLabel>
+										<FormControl>
+											<Input {...field} />
+										</FormControl>
+										<FormDescription>Enter user first name.</FormDescription>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="lastName"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Last Name</FormLabel>
+										<FormControl>
+											<Input {...field} />
+										</FormControl>
+										<FormDescription>Enter user last name.</FormDescription>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="username"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>User Name</FormLabel>
+										<FormControl>
+											<Input {...field} />
+										</FormControl>
+										<FormDescription>
+											Enter username for this user.
+										</FormDescription>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="emailAddress"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Email Addresses</FormLabel>
+										<FormControl>
+											<Input
+												{...field}
+												placeholder="email1@gmail.com, email2@gmail.com"
+												onChange={(e) => {
+													const emails = e.target.value
+														.split(",")
+														.map((email) => email.trim())
+														.filter((email) => email);
+
+													field.onChange(emails);
+												}}
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="password"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Password</FormLabel>
+										<FormControl>
+											<Input {...field} type="password" />
+										</FormControl>
+										<FormDescription>
+											Enter password for this user.
+										</FormDescription>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+
+							<Button
+								type="submit"
+								disabled={mutation.isPending}
+								className="disabled:opacity-50 disabled:cursor-not-allowed"
+							>
+								{mutation.isPending ? "Submitting..." : "Submit"}
+							</Button>
+						</form>
+					</Form>
+				</SheetDescription>
+			</SheetHeader>
+		</SheetContent>
+	);
 };
 
 export default AddUser;
